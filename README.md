7c3990df005f24324789f574fcd60cce728a49be
Akibur <akibur.choton@gmail.com>
2020-07-23 00:08:21 +0600

    Adding Templates, dynamic urls etc

--- README.md
+++
@@ -1,6 +0,0 @@
-# Blog Project in Django
-
-This is my 2nd project on Django framework. I am still learning and trying to understand how django works for different purpose.
-
-<h2>Dated: 22.07.2020</h2>
-Basic configuration of django views, urls, models etc. 
--- djangoBlogProject/urls.py
+++ djangoBlogProject/urls.py
@@ -1,7 +1,6 @@
-"""djangoBlogProject URL Configuration
-
+"""django_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +14,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
