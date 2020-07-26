042d5102bf7c7813157d5d882fadde971c31e737
Akibur <akibur.choton@gmail.com>
2020-07-26 18:21:02 +0600

    User Authentication, Profile Page, Pic, Signal etc

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -11,8 +12,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created and you are now able to login!')
+            return redirect('login')
             
     else:
         form    = UserRegisterForm()
@@ -21,3 +22,11 @@ def register(request):
         'form': form
     }
     return render(request, 'users/register.html', context)
+
+@login_required
+def profile(request):
+
+    context = {
+        
+    }
+    return render(request, 'users/profile.html', context)
